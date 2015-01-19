87e57808d0fafa9b30c0730c236f91591741e69c
Ivan Tertychnyy <ivantertychnyi@gmail.com>
2015-01-19 14:04:11 +0300

    path fix

---
+++ lib/rrckibackup/BKPConfig.py
@@ -0,0 +1,19 @@
+import os
+from configobj import ConfigObj
+
+class BKPConfig:
+    def __init__(self):
+        self.config = ConfigObj('conf/conf.ini')
+
+    def getLogDir(self):
+        return self.config['logdir']
+
+    def getHome(self):
+        return self.config['homedir']
+
+    def getServerHome(self):
+        return self.config['serverhome']
+
+    def getDataHome(self):
+        return self.config['datahome']
+
